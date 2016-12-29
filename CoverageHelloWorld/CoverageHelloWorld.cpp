c5349abf873a4aa8406f7292abe6e0dbc95f098b
xKubo <Peter.Kubini@gmail.com>
2016-12-29 21:44:13 +0100

    First commit

---
+++ CoverageHelloWorld/CoverageHelloWorld.cpp
@@ -0,0 +1,15 @@
+// CoverageHelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello world" << endl;
+    return 0;
+}
+
